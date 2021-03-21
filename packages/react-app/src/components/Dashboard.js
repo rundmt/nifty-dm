58b7295af8a19268520b4a537de5ea878adcf391
Derek Tor <derekmuoktor@gmail.com>
2021-03-20 17:05:14 -0700

    Adding new tokens with links

---
+++ packages/react-app/src/components/Dashboard.js
@@ -0,0 +1,128 @@
+import React from "react";
+import styled from "styled-components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCoins,
+  faWallet,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link, useHistory } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faCoins, faWallet, faComments);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ChatContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const TokenIcon = styled.div`
+  margin-right: 24px;
+`;
+
+const WalletIcon = styled.div`
+  width: 48px;
+  height: 48px;
+  background-color: green;
+  margin-right: 24px;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+`;
+
+const SectionHeader = styled.h2`
+  font-size: 24px;
+`;
+
+const Dashboard = ({ tokensOwned, tokensCreated }) => {
+  const history = useHistory();
+  console.log(history);
+  const tokens = React.useMemo(() => {
+    if (history.location.search === "?type=owned") {
+      return tokensOwned;
+    }
+
+    return tokensCreated;
+  }, [history.location.search, tokensCreated, tokensOwned]);
+
+  return (
+    <Container>
+      <Link
+        to={{
+          pathname: "/",
+          search: "?type=created",
+        }}
+      >
+        Created
+      </Link>
+      <Link
+        to={{
+          pathname: "/",
+          search: "?type=owned",
+        }}
+      >
+        Owned
+      </Link>
+      <SectionHeader>Tokens</SectionHeader>
+      <Section>
+        <ChatContainer>
+          {tokens.map((token) => {
+            return (
+              <TokenIcon>
+                {/* TODO get the wallet owner of the token to chat */}
+                <Link to="">
+                  <FontAwesomeIcon
+                    color="gold"
+                    size="2x"
+                    icon={["fas", "coins"]}
+                  />
+                  <div>{token.name}</div>
+                </Link>
+              </TokenIcon>
+            );
+          })}
+        </ChatContainer>
+      </Section>
+      <SectionHeader>Wallets</SectionHeader>
+      <Section>
+        <ChatContainer>
+          <TokenIcon>
+            <Link to={`/chat/`}>
+              <FontAwesomeIcon
+                color="brown"
+                size="2x"
+                icon={["fas", "wallet"]}
+              />
+            </Link>
+          </TokenIcon>
+        </ChatContainer>
+      </Section>
+      <SectionHeader>Group</SectionHeader>
+      <Section>
+        <ChatContainer>
+          <TokenIcon>
+            <Link to={`/chat/`}>
+              <FontAwesomeIcon
+                color="purple"
+                size="2x"
+                icon={["fas", "comments"]}
+              />
+            </Link>
+          </TokenIcon>
+        </ChatContainer>
+      </Section>
+    </Container>
+  );
+};
+
+export default Dashboard;
